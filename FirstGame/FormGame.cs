aac85e01f44bfe4356bbf7f2a5690bdad340820a
Taras Buhra <eroccked@gmail.com>
2018-04-30 15:22:39 +0300

    nachalo

---
+++ FirstGame/FormGame.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstGame
+{
+    public partial class FormGame : Form
+    {
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+
+            button(position).Text = position.ToString();
+
+            //MessageBox.Show(position.ToString());
+        }
+
+        private Button button (int position)
+        {
+            switch (position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default : return null;
+            }
+        }
+    }
+}
